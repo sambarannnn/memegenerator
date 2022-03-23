62b235f5b7443e1a0f7956f538507cdce0e0eb62
Sambaran Sengupta <sambaran55sengupta@icloud.com>
2022-03-24 00:28:58 +0530

    Create a React app and publish it to GitHub Pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h4 className="header--project">Practice Hooks!</h4>
         </header>
     )
 }
